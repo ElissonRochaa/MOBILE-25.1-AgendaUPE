539377e5de13a5c2c62e34b37092ba7a4f14e740
Renan <renanguilherme1941@hotmail.com>
2025-05-23 03:13:12 -0300

    eventos

--- calendarioupe/lib/screens/calendar_screeen.dart
+++ calendarioupe/lib/screens/calendar_screeen.dart
@@ -1,5 +1,13 @@
+import 'dart:collection';
+
+import 'package:calendarioupe/models/event_model.dart';
+import 'package:calendarioupe/utils/faker_api.dart';
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
+
+
+LinkedHashMap<DateTime, List<AppEvent>>? _groupedEvents;
+
 
 class CalendarScreeen extends StatefulWidget {
   const CalendarScreeen({super.key});
@@ -8,13 +16,58 @@ class CalendarScreeen extends StatefulWidget {
   State<CalendarScreeen> createState() => _CalendarScreeenState();
 }
 
+
+List<AppEvent> events = [];
+
 class _CalendarScreeenState extends State<CalendarScreeen> {
+
   DateTime today = DateTime.now();
   void _onDaySelected(DateTime day, DateTime focusedDay) {
     setState(() {
       today = day;
     });
   }
+
+
+
+@override
+void initState(){
+   addSchedules();
+
+}
+
+List<dynamic> _getEventsForDay(DateTime date){
+  return _groupedEvents?[date] ?? [];
+}
+
+Future addSchedules() async{
+  await FakerApi.getData().then((schedules){
+    for (var i=0; i< schedules.length; i++){
+      events.add(AppEvent(
+        date: DateTime.parse(schedules[i].birthday),
+        title: schedules[i].firstname));
+    }
+  } );
+
+  _groupEvents(events);
+}
+
+
+int getHashCode(DateTime key){
+  return key.day * 100000 +key.month * 100000 +key.year;
+}
+
+  _groupEvents(List<AppEvent> events){
+    _groupedEvents = LinkedHashMap(equals: isSameDay ,hashCode: getHashCode);
+    for (var event in events){
+      DateTime date = DateTime.utc(event.date!.year, event.date!.month, event.date!.day, 12);
+      if(_groupedEvents![date] == null) _groupedEvents![date]= [];
+      _groupedEvents![date]!.add(event);
+    }
+  }
+
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -23,10 +76,11 @@ class _CalendarScreeenState extends State<CalendarScreeen> {
     child: Column(
       children: [
         TableCalendar(
+          eventLoader: _getEventsForDay,
           selectedDayPredicate: (day) => isSameDay(day, today),
-          focusedDay: today, 
-          firstDay: DateTime.utc(2025, 1, 1), 
-          lastDay: DateTime.utc(2025, 12, 31),
+          focusedDay: DateTime.utc(2021, 12, 31), 
+          firstDay: DateTime.utc(2021, 1, 1), 
+          lastDay: DateTime.utc(2021, 12, 31),
           onDaySelected: _onDaySelected,
           headerStyle: HeaderStyle(
             formatButtonVisible: false,
@@ -36,7 +90,37 @@ class _CalendarScreeenState extends State<CalendarScreeen> {
           ),
           ),
 
-         Text(today.toString().split(" ")[0]),
+         Text(today.toString().split(" ")[0],
+         style: const TextStyle(
+                          color: Color.fromARGB(255, 0, 0, 0),
+                          fontSize: 100.0,),
+          textAlign: TextAlign.right,
+
+         ),
+
+          const SizedBox(height: 0.1),
+          Expanded(
+            child: GridView.count(
+              crossAxisCount: 5,
+              children: [
+                ..._getEventsForDay(today).map((event) => Card(
+                    color: Colors.blueGrey,
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(2.0)),
+                    ),
+                    child: ListTile(
+                      title: Text(
+                        event.title.toString(),
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 15.0,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    )))
+              ],
+            ),
+          ),
   
       ],
     )
